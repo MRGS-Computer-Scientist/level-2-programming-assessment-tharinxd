fa7420ba31f4a61a04a44673e8dc96cdbd517dd7
tharinxd <21027@students.mrgs.school.nz>
2024-05-15 14:45:05 +1200

    made a button

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from tkinter import *
 
-window = Tl()
-window.geomerty("400x400")
+w_width = 500
+w_height = 700
+
+window = Tk()
+window.geomerty(str(w_width) + "n" + str(w_height))
 window.title("My app")
 
+main_frame = Frame(background= "red" , width=w_width, height= w_height)
+main_frame.pack()
+
+hello_label = Label(text= "Hello Bilal")
+hello_label.grid(column=0, row=0)
+
+
 window.mainloop()
+
