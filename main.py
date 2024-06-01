712392a9d985092eae3fefc7a821ad76885e23c3
tharinxd <160798776+tharinxd@users.noreply.github.com>
2024-06-01 05:55:26 +0000

    restart

--- main.py
+++
@@ -1,9 +0,0 @@
-from app_settings import load_credentials
-from app import create_login_window
-
-# Load stored credentials
-load_credentials()
-print("Loaded users:", users)  # Print loaded users for debugging
-
-# Start the application
-create_login_window()
